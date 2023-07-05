7c35e66c4c005af73e69ecbf182ef49695880aae
denmarkgit <denmark.m.angeles@gmail.com>
2023-07-05 12:43:44 +1000

    updated

--- cron.sh
+++ cron.sh
@@ -5,5 +5,4 @@
 cd /home/denmark/gdc-pc-onboarding
 
 # ... commands executed under lock ...
-flock /tmp/ansible-playbook.lock /home/denmark/.local/bin/ansible-playbook main.yml >> /tmp/ansible-logs.txt 2>&1
-
+/home/denmark/.local/bin/ansible-playbook main.yml >> /tmp/ansible-logs.txt 2>&1
